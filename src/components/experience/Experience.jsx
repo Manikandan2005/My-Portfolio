99bc605bc99c400f46c16b2b871f0dda72c4a8f2
Manikandan R <115456961+Manikandan2005@users.noreply.github.com>
2024-04-03 01:04:29 +0530

    Update Experience.jsx

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -21,23 +21,26 @@ function Experience() {
     const ref = useRef();
     const isInView = useInView(ref, { margin: "-100px" });
   return (
-    <motion.div className="page" initial="initial"
-    variants={variants} 
-    ref={ref}
-    animate={isInView ? "animate" : "initial"}>
+    <div className="page">
         <div className="heading">
             <h1><i className="fa-solid fa-briefcase" style={{color: "#fafafa"}}/>Experience</h1>
         </div>
     <div className='experience'>
-        <div className="titleContainer" >
+        <motion.div className="titleContainer" initial="initial"
+    variants={variants} 
+    ref={ref}
+    animate={isInView ? "animate" : "initial"}>
           <div >
           <img src='./kaar.png' alt='KaarTech' ></img>
           </div>
-            <motion.h1 variants={variants} ><b>Kaar Technologies</b></motion.h1>
-            <motion.h3 variants={variants} >FTF Intern</motion.h3>
-            <motion.h5 variants={variants} >Oct 2023 - Present</motion.h5>
-        </div>
-        <div className="textContainer"  >
+            <h1 ><b>Kaar Technologies</b></h1>
+            <h3 >FTF Intern</h3>
+            <h5 >Oct 2023 - Present</h5>
+        </motion.div>
+        <motion.div className="textContainer" initial="initial"
+    variants={variants} 
+    ref={ref}
+    animate={isInView ? "animate" : "initial"}>
             <ul >
                 <li>Underwent trainings in Javascript, Angular, Node.js, MongoDB and developed websites using it.</li>
                 <li>Underwent trainings in SAP modules like SAP ABAP, PIPO, Odata, RFC.</li>
@@ -45,9 +48,9 @@ function Experience() {
                     understood the business process of each portal
                 </li>
             </ul>
-        </div>
+        </motion.div>
     </div>
-    </motion.div>
+    </div>
   )
 }
 
